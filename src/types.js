327e8fbd3ef321a35bf3b906175045ea3b1d991c
Jānis Elmeris <janise@inbox.lv>
2018-11-04 13:17:56 +0200

    - Add some type properties

--- src/types.js
+++ src/types.js
@@ -7,7 +7,16 @@ module.exports = {
 		dichotomyAbbr: 'ENFj',
 		socialRole: 'skolotājs',
 		quadra: 'β',
-		functions: ['Fe', 'Ni', 'Te', 'Si', 'Ti', 'Se', 'Fi', 'Ne']
+		functions: ['Fe', 'Ni', 'Te', 'Si', 'Ti', 'Se', 'Fi', 'Ne'],
+		professionalClub: 'humanitarians',
+		communicationStyle: 'passionate',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: true,
+			positiveNotNegative: false,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: false
+		}
 	},
 	EII: {
 		epithet: 'Dostoyevsky',
@@ -15,7 +24,16 @@ module.exports = {
 		dichotomyAbbr: 'INFj',
 		socialRole: 'humānists',
 		quadra: 'δ',
-		functions: ['Fi', 'Ne', 'Ti', 'Se', 'Te', 'Si', 'Fe', 'Ni']
+		functions: ['Fi', 'Ne', 'Ti', 'Se', 'Te', 'Si', 'Fe', 'Ni'],
+		professionalClub: 'humanitarians',
+		communicationStyle: 'sincere',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: false,
+			positiveNotNegative: true,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: false
+		}
 	},
 	ESE: {
 		epithet: 'Hugo',
@@ -23,7 +41,16 @@ module.exports = {
 		dichotomyAbbr: 'ESFj',
 		socialRole: 'iedvesmotājs',
 		quadra: 'α',
-		functions: ['Fe', 'Si', 'Te', 'Ni', 'Ti', 'Ne', 'Fi', 'Se']
+		functions: ['Fe', 'Si', 'Te', 'Ni', 'Ti', 'Ne', 'Fi', 'Se'],
+		professionalClub: 'socials',
+		communicationStyle: 'passionate',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: false,
+			positiveNotNegative: true,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: true
+		}
 	},
 	ESI: {
 		epithet: 'Dreiser',
@@ -31,7 +58,16 @@ module.exports = {
 		dichotomyAbbr: 'ISFj',
 		socialRole: 'sargātājs',
 		quadra: 'γ',
-		functions: ['Fi', 'Se', 'Ti', 'Ne', 'Te', 'Ni', 'Fe', 'Si']
+		functions: ['Fi', 'Se', 'Ti', 'Ne', 'Te', 'Ni', 'Fe', 'Si'],
+		professionalClub: 'socials',
+		communicationStyle: 'sincere',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: true,
+			positiveNotNegative: false,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: true
+		}
 	},
 	ILE: {
 		epithet: 'Don Quixote',
@@ -39,7 +75,16 @@ module.exports = {
 		dichotomyAbbr: 'ENTp',
 		socialRole: 'meklētājs',
 		quadra: 'α',
-		functions: ['Ne', 'Ti', 'Se', 'Fi', 'Si', 'Fe', 'Ni', 'Te']
+		functions: ['Ne', 'Ti', 'Se', 'Fi', 'Si', 'Fe', 'Ni', 'Te'],
+		professionalClub: 'researchers',
+		communicationStyle: 'businesslike',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: true,
+			positiveNotNegative: true,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: true
+		}
 	},
 	ILI: {
 		epithet: 'Balzac',
@@ -47,7 +92,16 @@ module.exports = {
 		dichotomyAbbr: 'INTp',
 		socialRole: 'kritiķis',
 		quadra: 'γ',
-		functions: ['Ni', 'Te', 'Si', 'Fe', 'Se', 'Fi', 'Ne', 'Ti']
+		functions: ['Ni', 'Te', 'Si', 'Fe', 'Se', 'Fi', 'Ne', 'Ti'],
+		professionalClub: 'researchers',
+		communicationStyle: 'coldblooded',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: false,
+			positiveNotNegative: false,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: true
+		}
 	},
 	IEE: {
 		epithet: 'Huxley',
@@ -55,7 +109,16 @@ module.exports = {
 		dichotomyAbbr: 'ENFp',
 		socialRole: 'komunikators',
 		quadra: 'δ',
-		functions: ['Ne', 'Fi', 'Se', 'Ti', 'Si', 'Te', 'Ni', 'Fe']
+		functions: ['Ne', 'Fi', 'Se', 'Ti', 'Si', 'Te', 'Ni', 'Fe'],
+		professionalClub: 'humanitarians',
+		communicationStyle: 'passionate',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: false,
+			positiveNotNegative: false,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: true
+		}
 	},
 	IEI: {
 		epithet: 'Yesenin',
@@ -63,7 +126,16 @@ module.exports = {
 		dichotomyAbbr: 'INFp',
 		socialRole: 'motivētājs',
 		quadra: 'β',
-		functions: ['Ni', 'Fe', 'Si', 'Te', 'Se', 'Ti', 'Ne', 'Fi']
+		functions: ['Ni', 'Fe', 'Si', 'Te', 'Se', 'Ti', 'Ne', 'Fi'],
+		professionalClub: 'humanitarians',
+		communicationStyle: 'sincere',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: true,
+			positiveNotNegative: true,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: true
+		}
 	},
 	LIE: {
 		epithet: 'Jack London',
@@ -71,7 +143,16 @@ module.exports = {
 		dichotomyAbbr: 'ENTj',
 		socialRole: 'uzņēmējs',
 		quadra: 'γ',
-		functions: ['Te', 'Ni', 'Fe', 'Si', 'Fi', 'Se', 'Ti', 'Ne']
+		functions: ['Te', 'Ni', 'Fe', 'Si', 'Fi', 'Se', 'Ti', 'Ne'],
+		professionalClub: 'researchers',
+		communicationStyle: 'businesslike',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: false,
+			positiveNotNegative: true,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: false
+		}
 	},
 	LII: {
 		epithet: 'Robespierre',
@@ -79,7 +160,16 @@ module.exports = {
 		dichotomyAbbr: 'INTj',
 		socialRole: 'analītiķis',
 		quadra: 'α',
-		functions: ['Ti', 'Ne', 'Fi', 'Se', 'Fe', 'Si', 'Te', 'Ni']
+		functions: ['Ti', 'Ne', 'Fi', 'Se', 'Fe', 'Si', 'Te', 'Ni'],
+		professionalClub: 'researchers',
+		communicationStyle: 'coldblooded',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: true,
+			positiveNotNegative: false,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: false
+		}
 	},
 	LSE: {
 		epithet: 'Stierlitz',
@@ -87,7 +177,16 @@ module.exports = {
 		dichotomyAbbr: 'ESTj',
 		socialRole: 'administrētājs',
 		quadra: 'δ',
-		functions: ['Te', 'Si', 'Fe', 'Ni', 'Fi', 'Ne', 'Ti', 'Se']
+		functions: ['Te', 'Si', 'Fe', 'Ni', 'Fi', 'Ne', 'Ti', 'Se'],
+		professionalClub: 'pragmatists',
+		communicationStyle: 'businesslike',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: true,
+			positiveNotNegative: false,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: true
+		}
 	},
 	LSI: {
 		epithet: 'Maxim Gorky',
@@ -95,7 +194,16 @@ module.exports = {
 		dichotomyAbbr: 'ISTj',
 		socialRole: 'sistematizētājs',
 		quadra: 'β',
-		functions: ['Ti', 'Se', 'Fi', 'Ne', 'Fe', 'Ni', 'Te', 'Si']
+		functions: ['Ti', 'Se', 'Fi', 'Ne', 'Fe', 'Ni', 'Te', 'Si'],
+		professionalClub: 'pragmatists',
+		communicationStyle: 'coldblooded',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: false,
+			positiveNotNegative: true,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: true
+		}
 	},
 	SLE: {
 		epithet: 'Zhukov',
@@ -103,7 +211,16 @@ module.exports = {
 		dichotomyAbbr: 'ESTp',
 		socialRole: 'rīkotājs',
 		quadra: 'β',
-		functions: ['Se', 'Ti', 'Ne', 'Fi', 'Ni', 'Fe', 'Si', 'Te']
+		functions: ['Se', 'Ti', 'Ne', 'Fi', 'Ni', 'Fe', 'Si', 'Te'],
+		professionalClub: 'pragmatists',
+		communicationStyle: 'businesslike',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: false,
+			positiveNotNegative: false,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: false
+		}
 	},
 	SLI: {
 		epithet: 'Gabin',
@@ -111,7 +228,16 @@ module.exports = {
 		dichotomyAbbr: 'ISTp',
 		socialRole: 'amatnieks',
 		quadra: 'δ',
-		functions: ['Si', 'Te', 'Ni', 'Fe', 'Ne', 'Fi', 'Se', 'Ti']
+		functions: ['Si', 'Te', 'Ni', 'Fe', 'Ne', 'Fi', 'Se', 'Ti'],
+		professionalClub: 'pragmatists',
+		communicationStyle: 'coldblooded',
+		dichotomies: {
+			aristocraticNotDemocratic: true,
+			askingNotDeclaring: true,
+			positiveNotNegative: true,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: false
+		}
 	},
 	SEE: {
 		epithet: 'Napoleon',
@@ -119,7 +245,16 @@ module.exports = {
 		dichotomyAbbr: 'ESFp',
 		socialRole: 'politiķis',
 		quadra: 'γ',
-		functions: ['Se', 'Fi', 'Ne', 'Ti', 'Ni', 'Te', 'Si', 'Fe']
+		functions: ['Se', 'Fi', 'Ne', 'Ti', 'Ni', 'Te', 'Si', 'Fe'],
+		professionalClub: 'socials',
+		communicationStyle: 'passionate',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: true,
+			positiveNotNegative: true,
+			subjectiveNotObjective: false,
+			tacticalNotStrategic: false
+		}
 	},
 	SEI: {
 		epithet: 'Dumas',
@@ -127,6 +262,15 @@ module.exports = {
 		dichotomyAbbr: 'ISFp',
 		socialRole: 'starpnieks',
 		quadra: 'α',
-		functions: ['Si', 'Fe', 'Ni', 'Te', 'Ne', 'Ti', 'Se', 'Fi']
+		functions: ['Si', 'Fe', 'Ni', 'Te', 'Ne', 'Ti', 'Se', 'Fi'],
+		professionalClub: 'socials',
+		communicationStyle: 'sincere',
+		dichotomies: {
+			aristocraticNotDemocratic: false,
+			askingNotDeclaring: false,
+			positiveNotNegative: false,
+			subjectiveNotObjective: true,
+			tacticalNotStrategic: false
+		}
 	}
 };
